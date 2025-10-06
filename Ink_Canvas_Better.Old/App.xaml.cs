4876d0830122038a2708eb7e2087b83cc642dc1d
ThreeMonthAgo <225839283+ThreeMonthAgo@users.noreply.github.com>
2025-10-06 17:41:11 +0800

    [change] Restart refactoring

---
+++ Ink_Canvas_Better.Old/App.xaml.cs
@@ -0,0 +1,69 @@
+﻿using Ink_Canvas_Better.Helpers;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using System.Windows;
+
+namespace Ink_Canvas_Better
+{
+    public partial class App : Application
+    {
+
+        public static string[]? StartArgs = null;
+        public static string RootPath = Environment.GetEnvironmentVariable("APPDATA") + "\\Ink Canvas Better\\";
+        public App()
+        {
+            this.DispatcherUnhandledException += App_DispatcherUnhandledException;
+            SystemEvents.UserPreferenceChanged += Setting.OnUserPreferenceChanged;
+
+            this.Startup += new StartupEventHandler(App_Startup);
+            this.Exit += new ExitEventHandler(App_OnExit);
+        }
+        private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            Log.NewLog(e.Exception.ToString());
+            // TODO: show in the messagebox
+            // Ink_Canvas.MainWindow.ShowNewMessage($"抱歉，出现预料之外的异常，可能导致 Ink Canvas 画板运行不稳定。\n建议保存墨迹后重启应用。\n报错信息：\n{e.ToString()}", true);
+            e.Handled = true;
+        }
+
+        void App_Startup(object sender, StartupEventArgs e)
+        {
+            RootPath = AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+
+            if (!Directory.Exists($"{RootPath}Logs"))
+            {
+                Directory.CreateDirectory($"{RootPath}Logs");
+            }
+
+            Log.NewLog(string.Format("Ink-Canvas-Better Starting (Version: {0})", Assembly.GetExecutingAssembly().GetName().Version?.ToString()));
+            
+
+            Mutex _ = new Mutex(true, "Ink_Canvas_Better", out bool ret);
+
+            if (!ret && !e.Args.Contains("-m")) // -m multiple
+            {
+                Log.NewLog("Detected existing instance");
+                MessageBox.Show("已有一个程序实例正在运行");
+                Log.NewLog("Ink-Canvas-Batter automatically closed");
+                Environment.Exit(0);
+            }
+
+            StartArgs = e.Args;
+        }
+
+        void App_OnExit(object sender, ExitEventArgs e)
+        {
+            SystemEvents.UserPreferenceChanged -= Setting.OnUserPreferenceChanged;
+            Log.NewLog("Ink-Canvas-Better exited");
+        }
+    }
+}
---
+++ Ink_Canvas_Better.Old/Resources/RuntimeData.cs
@@ -0,0 +1,195 @@
+﻿using Ink_Canvas_Better.Controls;
+using Ink_Canvas_Better.Windows;
+using Ink_Canvas_Better.Windows.FloatingBarIcons;
+using Newtonsoft.Json;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Ink;
+using System.Windows.Media;
+
+namespace Ink_Canvas_Better.ViewModel
+{
+    static class RuntimeData
+    {
+        #region instance
+
+        public static SettingWindow settingWindow;
+        public static MainWindow mainWindow;
+        public static FloatingBar_Pen floatingBar_Pen;
+        public static FloatingBar_Highlighter floatingBar_Highlighter;
+        public static ICB_ColorPicker colorPicker;
+
+        #endregion
+
+        #region Shape parameters
+
+        public static string CurrentShape { get; set; } = "Shape_Line";
+
+        public static bool IsShapeModePersistent { get; set; } = false;
+
+        public static int CurrentDrawStep = 0; // Shape
+
+        // special parameters for shapes
+        private static double Shape_Para_0;
+        public static double GetShapePara_0() { return Shape_Para_0; }
+        public static void UpdateShapePara_0() { Shape_Para_0 = Math.Round(Math.Log(mainWindow.MainInkCanvas.DefaultDrawingAttributes.Width + Math.E), 3); }
+
+        public static double Shape_Para_1;
+
+        #endregion
+
+        public static bool isCloseFromButton = false;
+        public static string settingsFileName = "settings.json";
+        public static Metadata currentMetadata = new Metadata();
+
+        public static DrawingAttributes CurrentDrawingAttributes_Pen { get; set; } = new DrawingAttributes()
+        {
+            Color = Color.FromRgb(255,0,0)
+        };
+        public static DrawingAttributes CurrentDrawingAttributes_Highlighter { get; set; } = new DrawingAttributes {
+            Color = Color.FromRgb(255,255,0),
+            StylusTip = StylusTip.Rectangle,
+            Width = 1
+        };
+
+        public static DrawingMode LastDrawingMode {  get; set; } = DrawingMode.None;
+
+        private static DrawingMode _currentDrawingMode = DrawingMode.None;
+        public static DrawingMode CurrentDrawingMode
+        {
+            get { return _currentDrawingMode; }
+            set
+            {
+                // Check
+                if (_currentDrawingMode != DrawingMode.Shape)
+                {
+                    mainWindow.CursorIcon.IsStatusEnable = false;
+                    mainWindow.PenIcon.IsStatusEnable = false;
+                    mainWindow.HighlighterIcon.IsStatusEnable = false;
+                    mainWindow.EraserIcon.IsStatusEnable = false;
+                    mainWindow.PickIcon.IsStatusEnable = false;
+                    LastDrawingMode = _currentDrawingMode;
+                }
+                else { mainWindow.ShapeIcon.IsStatusEnable = false; }
+                // Update _currentDrawingMode
+                _currentDrawingMode = value;
+                switch (_currentDrawingMode)
+                {
+                    case DrawingMode.None:
+                        break;
+                    case DrawingMode.Cursor:
+                        mainWindow.MainInkCanvas_Hitable = false;
+                        mainWindow.MainInkCanvas.EditingMode = InkCanvasEditingMode.None;
+                        mainWindow.CursorIcon.IsStatusEnable = true;
+                        break;
+                    case DrawingMode.Pen:
+                        mainWindow.MainInkCanvas_Hitable = true;
+                        mainWindow.MainInkCanvas.EditingMode = InkCanvasEditingMode.Ink;
+                        mainWindow.MainInkCanvas.DefaultDrawingAttributes = CurrentDrawingAttributes_Pen;
+                        mainWindow.PenIcon.IsStatusEnable = true;
+                        break;
+                    case DrawingMode.Highlighter:
+                        mainWindow.MainInkCanvas_Hitable = true;
+                        mainWindow.MainInkCanvas.EditingMode = InkCanvasEditingMode.Ink;
+                        mainWindow.MainInkCanvas.DefaultDrawingAttributes = CurrentDrawingAttributes_Highlighter;
+                        mainWindow.HighlighterIcon.IsStatusEnable = true;
+                        break;
+                    case DrawingMode.Eraser:
+                        mainWindow.MainInkCanvas_Hitable = true;
+                        if (CurrentEraserMode == EraserMode.Stroke) mainWindow.MainInkCanvas.EditingMode = InkCanvasEditingMode.EraseByStroke; else mainWindow.MainInkCanvas.EditingMode = InkCanvasEditingMode.EraseByPoint;
+                        mainWindow.MainInkCanvas.EraserShape = CurrentEraserShape;
+                        mainWindow.EraserIcon.IsStatusEnable = true;
+                        break;
+                    case DrawingMode.Pick:
+                        mainWindow.MainInkCanvas_Hitable = true;
+                        mainWindow.MainInkCanvas.EditingMode = InkCanvasEditingMode.Select;
+                        mainWindow.PickIcon.IsStatusEnable = true;
+                        break;
+                    case DrawingMode.Shape:
+                        mainWindow.MainInkCanvas_Hitable = true;
+                        mainWindow.MainInkCanvas.EditingMode = InkCanvasEditingMode.None;
+                        mainWindow.ShapeIcon.IsStatusEnable = true;
+                        break;
+                    default:
+                        var _ = _currentDrawingMode;
+                        _currentDrawingMode = DrawingMode.None;
+                        throw new NotImplementedException($"Unimplemented drawing mode: {_}");
+                }
+            }
+        }
+
+        private static void DefaultDrawingAttributes_PropertyDataChanged(object sender, PropertyDataChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static EraserMode CurrentEraserMode { get; set; } = EraserMode.Point;
+
+        public static StylusShape CurrentEraserShape { get; set; } = new EllipseStylusShape(30, 30);
+
+        public static void ApplyMetadata()
+        {
+            // TODO
+        }
+
+        public enum DrawingMode
+        {
+            None,
+            Cursor,
+            Pen,
+            Highlighter,
+            Eraser,
+            Pick, // Select
+            Shape
+        }
+
+        public enum EraserMode
+        {
+            Stroke,
+            Point
+        }
+
+    }
+
+    public class Metadata
+    {
+        [JsonProperty("metadataVer")]
+        public string MetadataVer { get; set; } = "1.0";
+
+        [JsonProperty("dateTime")]
+        public DateTime DateTime { get; set; }
+
+        [JsonProperty("icbMode")]
+        public ICBMode CurrentICBMode { get; set; } = ICBMode.None;
+
+
+
+        [JsonProperty("metadata_PPT")]
+        public Metadata_PPT ExInfo_PPT { get; set; } = new Metadata_PPT();
+
+        [JsonProperty("metadata_whiteBoard")]
+        public Metadata_whiteBoard ExInfo_whiteBoard { get; set; } = new Metadata_whiteBoard();
+        
+        public enum ICBMode
+        {
+            None,
+            PPT,
+            WhiteBoard
+        }
+    }
+
+    public class Metadata_PPT
+    {
+        [JsonProperty("slideCount")]
+        public int CurrentSlideIndex { get; set; } = 0;
+    }
+
+    public class Metadata_whiteBoard
+    {
+        [JsonProperty("slideCount")]
+        public int CurrentSlideIndex { get; set; } = 0;
+    }
+}
