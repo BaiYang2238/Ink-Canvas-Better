6d0dc6cc3afa57ea43147818bb1dfc3f89528b27
ThreeMonthAgo <225839283+ThreeMonthAgo@users.noreply.github.com>
2025-10-09 15:36:11 +0800

    temp

--- Ink_Canvas_Better/App.xaml.cs
+++ Ink_Canvas_Better/App.xaml.cs
@@ -1,12 +1,11 @@
 ﻿using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.IO;
 using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows;
-using Ink_Canvas_Better.Logging;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 
@@ -24,15 +23,19 @@ namespace Ink_Canvas_Better
         {
             this.Startup += new StartupEventHandler(App_Startup);
             this.Exit += new ExitEventHandler(App_OnExit);
+            Debug.WriteLine("hit - 1");
         }
 
         void App_Startup(object sender, StartupEventArgs e)
         {
             StartArgs = e.Args;
 
+            Debug.WriteLine("hit - 1");
+
             #region log
             IAppHost.InitAppHost();
             ILogger _logger = IAppHost.Host.Services.GetRequiredService<ILogger<App>>();
+            Debug.WriteLine("hit - 2");
             this.DispatcherUnhandledException += (sender, e) =>
             {
                 _logger.LogCritical(e.ToString());
@@ -40,10 +43,12 @@ namespace Ink_Canvas_Better
                 // Ink_Canvas.MainWindow.ShowNewMessage($"抱歉，出现预料之外的异常，可能导致 Ink Canvas 画板运行不稳定。\n建议保存墨迹后重启应用。\n报错信息：\n{e.ToString()}", true);
                 e.Handled = true;
             };
+            Debug.WriteLine("hit - 3");
             AppDomain.CurrentDomain.UnhandledException += (sender, e) =>
             {
                 _logger.LogWarning(e.ToString());
             };
+            Debug.WriteLine("hit - 4");
             TaskScheduler.UnobservedTaskException += (sender, e) =>
             {
                 _logger.LogError(e.ToString());
@@ -63,7 +68,7 @@ namespace Ink_Canvas_Better
             //    Log.NewLog("Ink-Canvas-Batter automatically closed");
             //    Environment.Exit(0);
             //}
-
+            Debug.WriteLine("hit - 5");
             _logger.LogInformation("===== Ink Canvas Better is running =====");
 
         }
